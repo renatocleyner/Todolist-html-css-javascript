ff8d1463972efbb33c0ffa9d07ebd1fdc9e2a7a6
renatocleyner <80468714+renatocleyner@users.noreply.github.com>
2023-01-16 07:42:20 -0300

    Commit inicial
    Projeto TodoList retirado de canal do youtube.

---
+++ todolist html css javascript/js/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let banco = [
+{'tarefa': 'Estudar JS', 'status': ''},
+{'tarefa': 'netflix', 'status': 'checked'}
+]
+
+const criarItem = (tarefa, status, indice) =>{
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML= `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+    `;
+    document.getElementById('todoList').appendChild(item);
+} 
+
+const limparTarefas=()=>{
+    const todoList = document.getElementById('todoList');
+    while(todoList.firstChild){
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) =>{
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if(tecla === 'Enter'){
+        banco.push({'tarefa': texto, 'status': ''});
+        atualizarTela();
+        evento.target.value = '';
+    }
+}
+const clickItem =(evento) =>{
+    const elemento = evento.target;
+    if(elemento.type === 'button'){
+        const indice = elemento.dataset.indice;
+        removerItem(indice);
+    }
+
+}
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+atualizarTela();
